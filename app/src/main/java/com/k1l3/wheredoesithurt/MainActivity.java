a20e9c62a11e5cb5be78f2aff8c73d2aeff2da7e
김호석 <hoseok76081024@gmail.com>
2019-06-30 13:03:21 +0900

    main화면 UI
    main화면 UI 뼈대구축

--- app/src/main/java/com/k1l3/wheredoesithurt/MainActivity.java
+++ app/src/main/java/com/k1l3/wheredoesithurt/MainActivity.java
@@ -1,13 +1,43 @@
 package com.k1l3.wheredoesithurt;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolbar;
+    ActionBar actionBar;
+    Fragment fragment_main;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowCustomEnabled(true); //커스터마이징 하기 위해 필요
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        replaceFragment(fragment_main=new Fragment_main());
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                //메뉴버튼 누를시 작동하게하는것
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void replaceFragment(@NonNull Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_container, fragment)
+                .commit();
     }
 }
