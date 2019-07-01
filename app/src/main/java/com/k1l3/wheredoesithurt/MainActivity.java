7cb3cae6fd52f41fbbe867cb2f8afe6c426b9b2a
HWP <sunghakzz@naver.com>
2019-07-01 15:51:46 +0900

    카카오 로그인 구현
    로그인 구현.

--- app/src/main/java/com/k1l3/wheredoesithurt/MainActivity.java
+++ app/src/main/java/com/k1l3/wheredoesithurt/MainActivity.java
@@ -1,13 +1,35 @@
 package com.k1l3.wheredoesithurt;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.security.MessageDigest;
+
+public class MainActivity extends Activity {
+
+    String strNickname, strProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tvNickname = findViewById(R.id.tvNickname);
+        TextView tvProfile = findViewById(R.id.tvProfile);
+
+        Intent intent = getIntent();
+        strNickname = intent.getStringExtra("name");
+        strProfile = intent.getStringExtra("profile");
+
+        tvNickname.setText(strNickname);
+        tvProfile.setText(strProfile);
     }
 }
