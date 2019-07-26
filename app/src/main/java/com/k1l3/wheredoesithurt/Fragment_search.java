a9b1b4248a672663ea5bd61fa06e4ff9f18306b3
김호석 <hoseok76081024@gmail.com>
2019-07-26 15:16:26 +0900

    search화면 재검색기능 추가
    search화면 재검색기능 추가

--- app/src/main/java/com/k1l3/wheredoesithurt/Fragment_search.java
+++ app/src/main/java/com/k1l3/wheredoesithurt/Fragment_search.java
@@ -9,13 +9,18 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -39,27 +44,42 @@ public class Fragment_search extends Fragment {
     Bitmap imgBitmap = null, no_medicine_image = null;
     FragmentManager manager;
     FragmentTransaction transaction;
+    EditText medicine_search;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         viewGroup = inflater.inflate(R.layout.fragment_search, container, false);
         listView = (ListView)viewGroup.findViewById(R.id.search_list_view);
+        medicine_search = (EditText)viewGroup.findViewById(R.id.medicin_search_2);
         manager = getActivity().getSupportFragmentManager();
         transaction = manager.beginTransaction();
 
         no_medicine_image = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.search_img);
         search_medicine_image = (ImageView)viewGroup.findViewById(R.id.search_medicine_image);
         adapter = new Adapter();
+        medicine_search.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        medicine_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(!medicine_search.getText().toString().equals("")) {
+                    setListView(medicine_search.getText().toString());
+                }
+                else{
+                    Toast.makeText(getContext(), "검색어를 입력해주세요", Toast.LENGTH_SHORT).show();
+                }
+                return false;
+            }
+        });
         if(getArguments()!=null){
             search_word = getArguments().getString("search_word");
 
-            new Thread(new Runnable() {
+           /* new Thread(new Runnable() {
 
                 @Override
                 public void run() {
                     // TODO Auto-generated method stub
-                    data= getXmlData();
+                    data= getXmlData(search_word);
                     for(int i=0;i<data.size();i++){
                         imgBitmap = GetImageFromURL(data.get(i).getItem_name());
                         if(imgBitmap!=null) {
@@ -97,7 +117,8 @@ public class Fragment_search extends Fragment {
                     }
 
                 }
-            }).start();
+            }).start();*/
+           setListView(search_word);
         }
         return viewGroup;
     }
@@ -106,10 +127,9 @@ public class Fragment_search extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
     }
-    private ArrayList<Search_item> getXmlData(){
+    private ArrayList<Search_item> getXmlData(String str){
 
         StringBuffer buffer=new StringBuffer();
-        String str= search_word;
         String item_name = null , entp_name = null, etc_otc_code = null, ee_doc_data = "", ud_doc_data = "";
         ArrayList<Search_item> item = new ArrayList<>();
         int stopping = 0;
@@ -293,5 +313,51 @@ public class Fragment_search extends Fragment {
         return imgBitmap;
     }
 
+    public void setListView(final String search_word){
 
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                data= getXmlData(search_word);
+                for(int i=0;i<data.size();i++){
+                    imgBitmap = GetImageFromURL(data.get(i).getItem_name());
+                    if(imgBitmap!=null) {
+                        data.get(i).setItem_image(imgBitmap);
+                    }
+                    else{
+                        data.get(i).setItem_image(no_medicine_image);
+                    }
+                }
+                if(getActivity()!=null){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            for(int i=0;i<data.size();i++){
+                                adapter.addItem(data);
+                                listView.setAdapter(adapter);
+                            }
+                        }
+                    });
+
+                    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            if(data.get(position).getItem_image()==no_medicine_image){
+                                data.get(position).setItem_image(null);
+                            }
+                            Fragment fragment_detail_search = new Fragment_detail_search();
+                            Bundle bundle = new Bundle();
+                            bundle.putSerializable("data", data.get(position));
+                            fragment_detail_search.setArguments(bundle);
+                            replaceFragment(fragment_detail_search);
+                        }
+                    });
+                }
+
+            }
+        }).start();
+    }
 }
