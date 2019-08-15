71fd05413a0ec167400dfa40c0ed8041f1072ee7
김호석 <hoseok76081024@gmail.com>
2019-08-16 01:19:09 +0900

    Search화면loading
    검색화면에 로딩구현

--- app/src/main/java/com/k1l3/wheredoesithurt/Fragment_search.java
+++ app/src/main/java/com/k1l3/wheredoesithurt/Fragment_search.java
@@ -19,9 +19,12 @@ import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.bumptech.glide.Glide;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
@@ -48,6 +51,9 @@ public class Fragment_search extends Fragment {
     EditText medicine_search;
     Toolbar toolBar;
     TextView backgroundchange;
+    ImageView loading;
+    ScrollView scrollView;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
@@ -58,7 +64,9 @@ public class Fragment_search extends Fragment {
         medicine_search = (EditText)viewGroup.findViewById(R.id.medicin_search_2);
         manager = getActivity().getSupportFragmentManager();
         transaction = manager.beginTransaction();
-
+        loading = (ImageView)viewGroup.findViewById(R.id.loading);
+        scrollView = (ScrollView)viewGroup.findViewById(R.id.scrollView3);
+        Glide.with(this).load(R.raw.loading).into(loading);
         no_medicine_image = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.search_img);
         search_medicine_image = (ImageView)viewGroup.findViewById(R.id.search_medicine_image);
         adapter = new Adapter();
@@ -279,6 +287,14 @@ public class Fragment_search extends Fragment {
             @Override
             public void run() {
                 // TODO Auto-generated method stub
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        loading.setVisibility(View.VISIBLE);
+                        scrollView.setVisibility(View.GONE);
+                    }
+                });
                 data= getXmlData(search_word);
                 for(int i=0;i<data.size();i++){
                     imgBitmap = GetImageFromURL(data.get(i).getItem_name());
@@ -298,6 +314,8 @@ public class Fragment_search extends Fragment {
                                 adapter.addItem(data);
                                 listView.setAdapter(adapter);
                             }
+                            scrollView.setVisibility(View.VISIBLE);
+                            loading.setVisibility(View.GONE);
                         }
                     });
 
